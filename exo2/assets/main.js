297a0b73739b761ac2d49241273200687d401ba4
NOUDOUKOU <121809959+Florasika@users.noreply.github.com>
2025-12-17 17:00:02 +0000

    .

---
+++ exo2/assets/main.js
@@ -0,0 +1,27 @@
+/*sélectionner un élément par son ID
+var html = document.getElementById("html")*/
+
+/*Sélectionner un élément dont la class est fournie en parametres(objet Document)
+var section = document.getElementsByClassName("section")*/
+
+/*l'élement dont le nom est fourni en paramètre
+document.getElementsByName("username")*/
+
+/* Sélectionner à partir de sa balise(getElementByTagName)
+var input = document.getElementsByTagName("input")*/
+
+/* Sélectionner à partir de leur sélecteur CSS fourni en paramètres
+var section = document.querySelector("section")/ querySelectorAll("section")*/
+
+//getAtribute pour récupérer et setAtribute pour modifier
+
+
+//innerHTML pour manipuler l'élément, textContent pour récupérer le contenu textuel d'un élément
+
+//Style pour modifier les propriétés CSS du doc
+var article = document.querySelectorAll('.article') [0]
+/*article.style.backgroundColor = "black"
+article.style.color = "pink"*/
+
+
+
